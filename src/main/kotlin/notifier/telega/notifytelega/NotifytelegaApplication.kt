d6fc6ed52c6944aabcb1c78e387bb920092feeb3
Fomin Mikhail <m.fomin@elama.ru>
2023-05-20 15:02:49 +0400

    init

---
+++ src/main/kotlin/notifier/telega/notifytelega/NotifytelegaApplication.kt
@@ -0,0 +1,11 @@
+package notifier.telega.notifytelega
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NotifytelegaApplication
+
+fun main(args: Array<String>) {
+	runApplication<NotifytelegaApplication>(*args)
+}
---
+++ src/test/kotlin/notifier/telega/notifytelega/NotifytelegaApplicationTests.kt
@@ -0,0 +1,13 @@
+package notifier.telega.notifytelega
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NotifytelegaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
